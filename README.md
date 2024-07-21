b0c87278a3d1306ea63112b38b0ddc2c383a3a4b
Cherubim <ch3rubim7@gmail.com>
2024-07-21 18:23:54 +0530

    initial code commit
    foundations have been laid ✌️

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # the-deck-of-proverbs
  
+A simple game to memorize the [proverbs](https://en.wikipedia.org/wiki/Book_of_Proverbs)
+
+Github page link: https://cherub7.github.io/the-deck-of-proverbs/
---
+++ js/script.js
@@ -0,0 +1,422 @@
+const proverbs = [
+    {
+        "beginning": "A gentle answer turns away wrath,",
+        "ending": "but a harsh word stirs up anger.",
+        "reference": "Proverbs 15:1"
+    },
+    {
+        "beginning": "Pride goes before destruction,",
+        "ending": "a haughty spirit before a fall.",
+        "reference": "Proverbs 16:18"
+    },
+    {
+        "beginning": "The fear of the Lord is the beginning of wisdom,",
+        "ending": "and knowledge of the Holy One is understanding.",
+        "reference": "Proverbs 9:10"
+    },
+    {
+        "beginning": "Trust in the Lord with all your heart",
+        "ending": "and lean not on your own understanding.",
+        "reference": "Proverbs 3:5"
+    },
+    {
+        "beginning": "A cheerful heart is good medicine,",
+        "ending": "but a crushed spirit dries up the bones.",
+        "reference": "Proverbs 17:22"
+    },
+    {
+        "beginning": "As iron sharpens iron,",
+        "ending": "so one person sharpens another.",
+        "reference": "Proverbs 27:17"
+    },
+    {
+        "beginning": "Better a little with righteousness",
+        "ending": "than much gain with injustice.",
+        "reference": "Proverbs 16:8"
+    },
+    {
+        "beginning": "A wise man listens to advice,",
+        "ending": "but a fool despises it.",
+        "reference": "Proverbs 12:15"
+    },
+    {
+        "beginning": "The path of the righteous is like the morning sun,",
+        "ending": "shining ever brighter till the full light of day.",
+        "reference": "Proverbs 4:18"
+    },
+    {
+        "beginning": "He who finds a wife finds what is good",
+        "ending": "and receives favor from the Lord.",
+        "reference": "Proverbs 18:22"
+    },
+    {
+        "beginning": "A fool gives full vent to his anger,",
+        "ending": "but a wise man keeps himself under control.",
+        "reference": "Proverbs 29:11"
+    },
+    {
+        "beginning": "Better a dry crust with peace and quiet",
+        "ending": "than a house full of feasting, with strife.",
+        "reference": "Proverbs 17:1"
+    },
+    {
+        "beginning": "The tongue has the power of life and death,",
+        "ending": "and those who love it will eat its fruit.",
+        "reference": "Proverbs 18:21"
+    },
+    {
+        "beginning": "A good name is more desirable than great riches;",
+        "ending": "to be esteemed is better than silver or gold.",
+        "reference": "Proverbs 22:1"
+    },
+    {
+        "beginning": "Like a city whose walls are broken through",
+        "ending": "is a person who lacks self-control.",
+        "reference": "Proverbs 25:28"
+    },
+    {
+        "beginning": "The Lord detests dishonest scales,",
+        "ending": "but accurate weights find favor with him.",
+        "reference": "Proverbs 11:1"
+    },
+    {
+        "beginning": "He who is slow to anger has great understanding,",
+        "ending": "but he who has a hasty temper exalts folly.",
+        "reference": "Proverbs 14:29"
+    },
+    {
+        "beginning": "A man’s wisdom gives him patience;",
+        "ending": "it is to his glory to overlook an offense.",
+        "reference": "Proverbs 19:11"
+    },
+    {
+        "beginning": "Whoever is patient has great understanding,",
+        "ending": "but one who is quick-tempered displays folly.",
+        "reference": "Proverbs 14:29"
+    },
+    {
+        "beginning": "Wine is a mocker, strong drink is raging:",
+        "ending": "and whosoever is deceived thereby is not wise.",
+        "reference": "Proverbs 20:1"
+    },
+    {
+        "beginning": "A rich man’s wealth is his strong city,",
+        "ending": "and like a high wall in his own conceit.",
+        "reference": "Proverbs 18:11"
+    },
+    {
+        "beginning": "The sluggard does not plow in the autumn;",
+        "ending": "he will seek at harvest and have nothing.",
+        "reference": "Proverbs 20:4"
+    },
+    {
+        "beginning": "Better a little with the fear of the Lord",
+        "ending": "than great wealth with turmoil.",
+        "reference": "Proverbs 15:16"
+    },
+    {
+        "beginning": "A wise son brings joy to his father,",
+        "ending": "but a foolish son brings grief to his mother.",
+        "reference": "Proverbs 10:1"
+    },
+    {
+        "beginning": "A person who is not wise in his own eyes,",
+        "ending": "but fears the Lord, is considered wise.",
+        "reference": "Proverbs 3:7"
+    },
+    {
+        "beginning": "The plans of the diligent lead to profit",
+        "ending": "as surely as haste leads to poverty.",
+        "reference": "Proverbs 21:5"
+    },
+    {
+        "beginning": "A man who isolates himself seeks his own desire;",
+        "ending": "he rages against all wise judgment.",
+        "reference": "Proverbs 18:1"
+    },
+    {
+        "beginning": "As water reflects the face,",
+        "ending": "so one’s life reflects the heart.",
+        "reference": "Proverbs 27:19"
+    },
+    {
+        "beginning": "He who covers over an offense promotes love,",
+        "ending": "but whoever repeats the matter separates close friends.",
+        "reference": "Proverbs 17:9"
+    },
+    {
+        "beginning": "Do not answer a fool according to his folly,",
+        "ending": "or you yourself will be just like him.",
+        "reference": "Proverbs 26:4"
+    },
+    {
+        "beginning": "The lips of the righteous know what is acceptable,",
+        "ending": "but the mouth of the wicked knows only what is perverse.",
+        "reference": "Proverbs 10:32"
+    },
+    {
+        "beginning": "The wise in heart are called discerning,",
+        "ending": "and gracious words promote instruction.",
+        "reference": "Proverbs 16:21"
+    },
+    {
+        "beginning": "Those who guard their lips preserve their lives,",
+        "ending": "but those who speak rashly will come to ruin.",
+        "reference": "Proverbs 13:3"
+    },
+    {
+        "beginning": "The way of the Lord is a refuge for the blameless,",
+        "ending": "but it is the ruin of those who do evil.",
+        "reference": "Proverbs 10:29"
+    },
+    {
+        "beginning": "A good man leaves an inheritance to his children’s children,",
+        "ending": "but the sinner’s wealth is stored up for the righteous.",
+        "reference": "Proverbs 13:22"
+    },
+    {
+        "beginning": "He who is kind to the poor lends to the Lord,",
+        "ending": "and he will reward him for what he has done.",
+        "reference": "Proverbs 19:17"
+    },
+    {
+        "beginning": "He who finds a wife finds what is good",
+        "ending": "and receives favor from the Lord.",
+        "reference": "Proverbs 18:22"
+    },
+    {
+        "beginning": "Whoever loves discipline loves knowledge,",
+        "ending": "but whoever hates correction is stupid.",
+        "reference": "Proverbs 12:1"
+    },
+    {
+        "beginning": "Plans fail for lack of counsel,",
+        "ending": "but with many advisers they succeed.",
+        "reference": "Proverbs 15:22"
+    },
+    {
+        "beginning": "A man of knowledge uses words with restraint,",
+        "ending": "and a man of understanding is even-tempered.",
+        "reference": "Proverbs 17:27"
+    },
+    {
+        "beginning": "The lazy do not roast any game,",
+        "ending": "but the diligent feed on the riches of the hunt.",
+        "reference": "Proverbs 12:27"
+    },
+    {
+        "beginning": "Do not withhold discipline from a child;",
+        "ending": "if you punish them with the rod, they will not die.",
+        "reference": "Proverbs 23:13"
+    },
+    {
+        "beginning": "A hot-tempered person stirs up conflict,",
+        "ending": "but the one who is patient calms a quarrel.",
+        "reference": "Proverbs 15:18"
+    },
+    {
+        "beginning": "The generous will themselves be blessed,",
+        "ending": "for they share their food with the poor.",
+        "reference": "Proverbs 22:9"
+    },
+    {
+        "beginning": "A wise servant will rule over a disgraceful son",
+        "ending": "and will share the inheritance as one of the family.",
+        "reference": "Proverbs 17:2"
+    },
+    {
+        "beginning": "The way of fools seems right to them,",
+        "ending": "but the wise listen to advice.",
+        "reference": "Proverbs 12:15"
+    },
+    {
+        "beginning": "Even fools are thought wise if they keep silent,",
+        "ending": "and discerning if they hold their tongues.",
+        "reference": "Proverbs 17:28"
+    },
+    {
+        "beginning": "The heart of the discerning acquires knowledge;",
+        "ending": "the ears of the wise seek it out.",
+        "reference": "Proverbs 18:15"
+    },
+    {
+        "beginning": "The tongue of the wise adorns knowledge,",
+        "ending": "but the mouth of the fool gushes folly.",
+        "reference": "Proverbs 15:2"
+    },
+    {
+        "beginning": "A false witness will perish,",
+        "ending": "but a careful listener will testify successfully.",
+        "reference": "Proverbs 21:28"
+    },
+    {
+        "beginning": "A wise man’s heart guides his mouth,",
+        "ending": "and his lips promote instruction.",
+        "reference": "Proverbs 16:23"
+    },
+    {
+        "beginning": "The wicked flee though no one pursues,",
+        "ending": "but the righteous are as bold as a lion.",
+        "reference": "Proverbs 28:1"
+    },
+    {
+        "beginning": "The Lord is far from the wicked,",
+        "ending": "but he hears the prayer of the righteous.",
+        "reference": "Proverbs 15:29"
+    },
+    {
+        "beginning": "A man’s wisdom gives him patience;",
+        "ending": "it is to his glory to overlook an offense.",
+        "reference": "Proverbs 19:11"
+    },
+    {
+        "beginning": "Better a little with righteousness",
+        "ending": "than much gain with injustice.",
+        "reference": "Proverbs 16:8"
+    },
+    {
+        "beginning": "The fear of the Lord is the beginning of wisdom,",
+        "ending": "and knowledge of the Holy One is understanding.",
+        "reference": "Proverbs 9:10"
+    },
+    {
+        "beginning": "A fool shows his annoyance at once,",
+        "ending": "but a prudent man overlooks an insult.",
+        "reference": "Proverbs 12:16"
+    },
+    {
+        "beginning": "The Lord’s blessing is on the head of the righteous,",
+        "ending": "but the mouth of the wicked conceals violence.",
+        "reference": "Proverbs 10:6"
+    },
+    {
+        "beginning": "The way of the Lord is a refuge for the blameless,",
+        "ending": "but it is the ruin of those who do evil.",
+        "reference": "Proverbs 10:29"
+    }
+]
+
+
+let flippedCards = [];
+let matchedPairs = 0;
+let matchCount = 0;
+let score = 0;
+
+function getRandomElements(arr, numElements) {
+    // Create a copy of the array to avoid mutating the original
+    let shuffledArray = arr.slice();
+    
+    // Shuffle the array using the Fisher-Yates algorithm
+    for (let i = shuffledArray.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [shuffledArray[i], shuffledArray[j]] = [shuffledArray[j], shuffledArray[i]];
+    }
+
+    // Return the first `numElements` from the shuffled array
+    return shuffledArray.slice(0, numElements);
+}
+
+function removeNonAlphabeticEnd(str) {
+    // Check if the last character is not an alphabetic character
+    if (str && !/[a-zA-Z]/.test(str.charAt(str.length - 1))) {
+        // Remove the last character
+        str = str.slice(0, -1);
+    }
+    return str;
+}
+
+function createCard(text, type) {
+    const card = document.createElement('div');
+    card.classList.add('card');
+    card.dataset.type = type;
+
+    const content = document.createElement('div');
+    content.classList.add('content');
+
+    const p = document.createElement('p');
+    p.textContent = removeNonAlphabeticEnd(text);
+
+    content.appendChild(p)
+    card.addEventListener('click', onCardClicked);
+    card.appendChild(content)
+    return card;
+}
+
+function onCardClicked(event) {
+    const clickedCard = event.currentTarget;
+
+    if (flippedCards.length < 2 && !clickedCard.classList.contains('flipped')) {
+        clickedCard.classList.add('flipped');
+        flippedCards.push(clickedCard);
+
+        if (flippedCards.length === 2) {
+            checkForMatch();
+        }
+    }
+}
+
+function checkForMatch() {
+    const [card1, card2] = flippedCards;
+
+    if (card1.dataset.type === card2.dataset.type) {
+        matchedPairs++;
+        score++;
+        const scoreCounter = document.getElementById('score-counter')
+        scoreCounter.hidden = false
+        scoreCounter.innerText = score;
+        flippedCards = [];
+
+        if (matchedPairs === matchCount) {
+            reInitializeGameBoard()
+        }
+    } else {
+        setTimeout(() => {
+            card1.classList.remove('flipped');
+            card2.classList.remove('flipped');
+            flippedCards = [];
+        }, 500);
+    }
+}
+
+function initializeGameBoard() {
+    flippedCards = []
+    matchedPairs = 0
+    matchCount = Math.min(matchCount+1, 3)
+
+    const gameBoard = document.getElementById('game-board');
+    gameBoard.innerHTML = '';
+
+    const cards = [];
+    const selectedProverbs = getRandomElements(proverbs, matchCount)
+    selectedProverbs.forEach((proverb, index) => {
+        cards.push(createCard(proverb.beginning, `card-${index}`));
+        cards.push(createCard(proverb.ending, `card-${index}`));
+    });
+
+    cards.sort(() => 0.5 - Math.random());
+    cards.forEach(card => gameBoard.appendChild(card));
+    cards.forEach(card => {
+        card.classList.add('appear');
+    });
+}
+
+initializeGameBoard();
+
+function reInitializeGameBoard() {
+    const cards = document.querySelectorAll('.card');
+    cards.forEach(card => {
+        card.classList.remove('appear')
+        card.classList.add('fade-out');
+    });
+    
+    // Wait for the animation to complete before reloading new cards
+    setTimeout(() => {
+        // Logic to reload new cards
+        initializeGameBoard();
+    }, 1000);
+}
+
+// TODO:
+// 1. display reference
+// 2. add more proverbs
+// 3. different versions/ languages
